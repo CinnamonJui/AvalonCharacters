0866f990345c1e5e9678da41175b0366a6abf8b6
Jie, Luo <rager1958@gmail.com>
2020-05-16 19:28:32 +0800

    Change positive & negative button in ChoiceCountDown view to Radio button

--- app/src/main/java/com/example/avaloncharacters/ChoiceCountDownView.kt
+++ app/src/main/java/com/example/avaloncharacters/ChoiceCountDownView.kt
@@ -6,7 +6,6 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.View
 import android.widget.FrameLayout
-import android.widget.Toast
 import com.example.avaloncharacters.characters.Player
 import com.example.avaloncharacters.characters.goodside.GoodSide
 import kotlinx.android.synthetic.main.choice_count_down_view.view.*
@@ -19,41 +18,38 @@ class ChoiceCountDownView : FrameLayout {
 
     private val GAMEPHASES: Array<String> = context.resources.getStringArray(R.array.game_phases)
     private lateinit var player: Player
-    private var choiceType: Int = 0
     fun setPlayer(p: Player) {
         player = p
     }
 
-    init {
-        View.inflate(context, R.layout.choice_count_down_view, this).post {
-            btnPositive.setOnClickListener {
-                it.setBackgroundResource(R.color.btn_positive)
-                btnNegative.setBackgroundResource(R.color.btn_neutral)
-                confirm.isEnabled = true
-            }
-            btnNegative.setOnClickListener {
-                if (choiceType == MISSION && player.character is GoodSide){
-                    Toast.makeText(context, "Only evil side can let mission fail!", Toast.LENGTH_SHORT).show()
-                    return@setOnClickListener
-                }
-                it.setBackgroundResource(R.color.btn_negative)
-                btnPositive.setBackgroundResource(R.color.btn_neutral)
-                confirm.isEnabled = true
-            }
-            this.visibility = View.INVISIBLE
-            startCountDown(MISSION, 8)
+    private var choiceType: Int = 0
 
+    init {
+        View.inflate(context, R.layout.choice_count_down_view, this)
+        btnPositive.setOnClickListener {
+            confirm.isEnabled = true
         }
+        btnNegative.setOnClickListener {
+            confirm.isEnabled = true
+        }
+        this.visibility = View.INVISIBLE
     }
 
 
     fun startCountDown(type: Int, seconds: Int, callback: (() -> Unit)? = null) {
         choiceType = type
 
+        confirm.isEnabled = false
+        btnNegative.visibility = View.VISIBLE
+        val goodsideEnterMission = choiceType == MISSION && player.character is GoodSide
+        if (goodsideEnterMission) {
+            confirm.isEnabled = true
+            btnNegative.visibility = View.GONE
+            btnPositive.isChecked = true
+        }
         countDownProgressBar.progress = 100
         countDownSeconds.text = seconds.toString()
         titleText.text = GAMEPHASES[type]
-        confirm.isEnabled = false
 
         visibility = View.VISIBLE
         (object : CountDownTimer(seconds * 1000L, 1000L) {
