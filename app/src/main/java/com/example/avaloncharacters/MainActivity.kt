e18718ea75bca832f477917cc9317fd761a02ce3
Jie, Luo <rager1958@gmail.com>
2020-05-19 23:24:36 +0800

    Set application to immersive mode

--- app/src/main/java/com/example/avaloncharacters/MainActivity.kt
+++ app/src/main/java/com/example/avaloncharacters/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.example.avaloncharacters
 
 import android.os.Bundle
+import android.view.View
+import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
-import com.example.avaloncharacters.helpers.PermissionHelper
+
+fun hideSystemUI(window: Window) {
+    window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            // Set the content to appear under the system bars so that the
+            // content doesn't resize when the system bars hide and show.
+            or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            // Hide the nav bar and status bar
+            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_FULLSCREEN)
+}
 
 class MainActivity : AppCompatActivity() {
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus)
+            hideSystemUI(window)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        supportFragmentManager.beginTransaction().apply {
-            add(PermissionHelper(), "Permission")
-            commitNow()
-            // This fragment will be auto destroyed
-        }
     }
 }
