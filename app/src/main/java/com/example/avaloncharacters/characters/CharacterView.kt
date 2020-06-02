8e1fc3b6f1f6097186d7b44946d29e010203bae3
hannn0919 <44270736+hannn0919@users.noreply.github.com>
2020-06-02 23:18:02 +0800

    fix magical number & use color.xml

--- app/src/main/java/com/example/avaloncharacters/characters/CharacterView.kt
+++ app/src/main/java/com/example/avaloncharacters/characters/CharacterView.kt
@@ -6,6 +6,7 @@ import android.graphics.drawable.GradientDrawable
 import android.util.AttributeSet
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import com.example.avaloncharacters.R
 import kotlinx.android.synthetic.main.character_view.view.*
 
@@ -18,25 +19,30 @@ class CharacterView : ConstraintLayout{
         View.inflate(context, R.layout.character_view, this)
     }
 
-    // change character color and number       # 1: evilside, 2: special, 3: unknown
-    fun setCharacter(num: Int, type: Int) {
+    // change character color and number
+    fun setCharacterIdentit(num: Int, type: Int) {
 
         character_circle.setText(num.toString())
 
         val grad: GradientDrawable = character_circle.background as GradientDrawable
-
-        if(type == 1){
-            grad.setColor(Color.parseColor("#EA0000"))
-        } else if(type == 2){
-            grad.setColor(Color.parseColor("#00A600"))
+        if(type == evilside){
+            grad.setColor(ContextCompat.getColor(context, R.color.circle_evil))
+        } else if(type == special){
+            grad.setColor(ContextCompat.getColor(context, R.color.circle_special))
         } else{
-            grad.setColor(Color.parseColor("#BEBEBE"))
+            grad.setColor(ContextCompat.getColor(context, R.color.circle_unknown))
         }
 
     }
 
     fun isChoosed(): Boolean {
         return character_ring.isChecked
+    }
+
+    companion object {
+        const val evilside = 1
+        const val special = 2
+        const val unknown = 3
     }
 
 }
