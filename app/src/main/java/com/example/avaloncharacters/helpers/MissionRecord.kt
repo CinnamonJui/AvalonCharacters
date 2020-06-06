7bd6a4380d06859a04b0e40269ffdd898064fd9e
shanlee870502 <alicelee870502@gmail.com>
2020-06-07 01:21:22 +0800

    feat: finish mission-record

--- app/src/main/java/com/example/avaloncharacters/helpers/MissionRecord.kt
+++ app/src/main/java/com/example/avaloncharacters/helpers/MissionRecord.kt
@@ -4,57 +4,97 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import android.widget.RelativeLayout
+import android.widget.TableRow
+import android.widget.TextView
 import com.example.avaloncharacters.R
-import com.example.avaloncharacters.characters.Player
 import kotlinx.android.synthetic.main.mission_record.view.*
 
-class MissionRecord:RelativeLayout {
+class MissionRecord : RelativeLayout {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
 
-    private lateinit var player: Player
-
     init {
         View.inflate(context, R.layout.mission_record, this)
-        mission_title.text="" // mission 1,2,3...
-        mission_detail.text="" //details of mission: the player who assign mission and assigned player
-        mission_result.text="" //pass or not pass
-        mission_result.setBackgroundColor(context.getColor(R.color.backgroud_default))
-        player1.text = ""
-        player2.text = ""
-        player3.text = ""
-        player4.text = ""
-        player5.text = ""
-        this.visibility = View.INVISIBLE
+        mission_result.text = "" //pass or not pass
+        mission_result.setBackgroundColor(getResources().getColor(R.color.background_default))
     }
 
-    fun setMissionTitle(missionIndex: Int)
-    {
-        var title:String="任務"
-        mission_title.text = title+ missionIndex.toString()
+    /**
+     * set mission title
+     * @param missionIndex The index of mission(will go through at most 5 mission)
+     */
+    fun setMissionTitle(missionIndex: Int) {
+        mission_title.text = context.getString(R.string.mission_title, missionIndex)
     }
 
-    fun setMissionDetail(mission_player:Array<String>)
-    {
-        //exp: mission_player=[1,3,5]
-        var detailMessage:String=""
-        detailMessage += mission_player[0]+" 派 "
-
-        for (player in 1 until mission_player.size){
-            detailMessage+=mission_player[player]+"  "
+    /**
+     * set mission table data
+     * @param total_round The rounds of voting in single mission
+     * @param vote_record Vote record of each round and each player
+     * @param king Who assign the mission
+     * @param assignee Who being assigned the mission by king
+     */
+//    , assignee: Array<Int>
+    fun setMissionTable(total_round: Int, vote_record: Array<Array<Int>>, king:Array<Int>, assignee:Array<Array<Int>>) {
+        for (round in 0 until total_round) {
+            // set new row which represent a round
+            val newRow = TableRow(context)
+            newRow.id = round
+            newRow.layoutParams = TABLE_ROW_LAYOUT_PARAM
+            // set round name according to round index(round index = 1,2...)
+            val roundTxt = TextView(context)
+            val roundIndex:Int = round+1
+            roundTxt.text = context.getString(R.string.round_name, roundIndex)
+            roundTxt.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            newRow.addView(roundTxt)
+            // set each player's vote data each round
+            for ((index, vote) in vote_record[round].withIndex()) {
+                val newData = TextView(context)
+                newData.text = VOTE_SIGN[vote]
+                newData.textAlignment = View.TEXT_ALIGNMENT_TEXT_START
+                //set king icon
+                if((index+1) == king[round]) {
+                    newData.compoundDrawablePadding = 0
+                    newData.setCompoundDrawablesWithIntrinsicBounds(R.drawable.crown, 0, 0, 0);
+                }
+                //set assignee
+                if(assignee[round].contains(index+1)) {
+                    newData.setBackgroundColor(getResources().getColor(R.color.assignee))
+                }
+                newRow.addView(newData)
+            }
+            // add new row to table
+            mission_table.addView(newRow)
         }
-        detailMessage +="出任務"
-
     }
 
-    fun setVoteResult(vote:Array<String>)
-    {
-        //exp: vote=[0,1,1,0,0], index=0 represents player 1
-        //0 means player disagree mission, 1 means player agree the mission
+    /**
+     * set Mission Result
+     * @param state The state of mission(success/fail/voting)
+     */
+    fun setMissionResult(state: Int) {
+        if (state == SUCCESS) {
+            mission_result.text = "通過"
+            mission_result.setBackgroundColor(getResources().getColor(R.color.background_success))
+        } else if (state == FAIL) {
+            mission_result.text = "未通過"
+            mission_result.setBackgroundColor(getResources().getColor(R.color.background_failed))
+        } else if (state == VOTING) {
+            mission_result.text = "投票中"
+            mission_result.setBackgroundColor(getResources().getColor(R.color.background_default))
+        }
+    }
 
-        //Following are the setVoteResult function work:
-        //1. update the O/X in the table
-        //2. show mission is success or not
-        //3. change the background color(background_success/background_failed)
+    companion object {
+        const val SUCCESS = 1
+        const val FAIL = -1
+        const val VOTING = 0
+
+        val TABLE_ROW_LAYOUT_PARAM = TableRow.LayoutParams(
+            TableRow.LayoutParams.WRAP_CONTENT,
+            TableRow.LayoutParams.WRAP_CONTENT
+        )
+
+        val VOTE_SIGN = arrayOf("X", "O")
     }
 }
